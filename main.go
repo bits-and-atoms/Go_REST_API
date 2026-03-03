0a1a1b811524d96e73c8875eafb176b4ab63a75f
subham <majumdersubham14082003@gmail.com>
2026-03-03 13:49:33 +0530

    get an event

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/bits-and-atoms/Go_REST_API/db"
 	"github.com/bits-and-atoms/Go_REST_API/model"
@@ -14,6 +15,7 @@ func main(){
 		ctx.JSON(http.StatusOK,gin.H{"message":"healthy"})
 	})
 	server.GET("/events", getEvents)
+	server.GET("/events/:id",getEvent)
 	server.POST("/events",createEvent)
 	server.Run(":8080")
 }
@@ -26,6 +28,23 @@ func getEvents(ctx *gin.Context){
 		return
 	}
 	ctx.JSON(http.StatusOK,events)
+}
+func getEvent(ctx *gin.Context){
+	id,err := strconv.ParseInt(ctx.Param("id"),10,64)
+	if err != nil{
+		ctx.JSON(http.StatusBadRequest,gin.H{
+			"message":"could not parse event id",
+		})
+		return 
+	}
+	event,err := model.GetEventById(id)
+	if err != nil{
+		ctx.JSON(http.StatusInternalServerError,gin.H{
+			"message":"could not fetch event or event not found",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK,event)
 }
 func createEvent(ctx *gin.Context){
 	var event model.Event
--- model/event.go
+++ model/event.go
@@ -5,62 +5,91 @@ import (
 
 	"github.com/bits-and-atoms/Go_REST_API/db"
 )
+
 // exec used for insert,update ,delete create
 //query used for select
 //prepare can be used with both exec and query, it is for performance , so if you dont close it then it saves
 //the query in memory so increases speed but if you close it after that it behaves as non prepare exec or query only
 
-type Event struct{
-	ID int64
-	Name string `binding:"required"`
-	Description string `binding:"required"`
-	Location string `binding:"required"`
-	DateTime time.Time `binding:"required"`
-	UserID int
+type Event struct {
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
 }
-func (e *Event) Save() error{
+
+func (e *Event) Save() error {
 	query := `
 	insert into events(name,description,location,dateTime,user_id)
 	values (?,?,?,?,?)
 	`
-	// this ? way prevents sql injection 
-	st,err := db.DB.Prepare(query)
-	defer st.Close()
-	if err != nil{
+	// this ? way prevents sql injection
+	st, err := db.DB.Prepare(query)
+	if err != nil {
+		defer st.Close()
 		return err
 	}
-	result, err := st.Exec(e.Name,e.Description,e.Location,e.DateTime,e.UserID)
+	result, err := st.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
 	//exec used for INSERT, UPDATE, DELETE and create
-	if err != nil{
+	if err != nil {
 		return err
 	}
-	id,err := result.LastInsertId()
-	if err != nil{
+	id, err := result.LastInsertId()
+	if err != nil {
 		return err
 	}
 	e.ID = id
 	return nil
 }
-func GetAllEvents() ([]Event,error){
+func GetAllEvents() ([]Event, error) {
 	query := "select * from events"
-	st,err := db.DB.Prepare(query)
-	defer st.Close()
-	if err != nil{
-		return nil,err
+	st, err := db.DB.Prepare(query)
+	if err != nil {
+		defer st.Close()
+		return nil, err
 	}
-	result,err := st.Query()
+	result, err := st.Query()
 	//query used for select
-	if err != nil{
-		return nil,err
+	if err != nil {
+		return nil, err
 	}
 	var events []Event
-	for result.Next(){
+	for result.Next() {
 		var curr Event
-		err := result.Scan(&curr.ID,&curr.Name,&curr.Description,&curr.Location,&curr.DateTime,&curr.UserID)
-		if err != nil{
-			return nil,err
+		err := result.Scan(&curr.ID, &curr.Name, &curr.Description, &curr.Location, &curr.DateTime, &curr.UserID)
+		if err != nil {
+			return nil, err
 		}
 		events = append(events, curr)
 	}
-	return events,nil
+	return events, nil
+}
+func GetEventById(id int64) (*Event, error) {
+	// without prepare
+	// query := "select * from events where id = ?"
+	// result := db.DB.QueryRow(query, id)
+	// var curr Event
+	// err := result.Scan(&curr.ID, &curr.Name, &curr.Description, &curr.Location, &curr.DateTime, &curr.UserID)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return &curr,nil
+	
+	//with prepare
+	query := "select * from events where id = ?"
+	st, err := db.DB.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	defer st.Close()
+
+	result := st.QueryRow(id)
+	var curr Event
+	err = result.Scan(&curr.ID, &curr.Name, &curr.Description, &curr.Location, &curr.DateTime, &curr.UserID)
+	if err != nil {
+		return nil, err
+	}
+	return &curr, nil
 }
